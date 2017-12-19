f59fdfde4de97680d856ba8ced5a5dfdd3d474b8
nooryu11 <31179504+nooryu11@users.noreply.github.com>
2017-12-19 08:01:07 -0500

    Add files via upload

---
+++ secret/secretFile.js
@@ -0,0 +1,12 @@
+module.exports={
+	facebook: {
+
+		clientID: '123541251649721',
+		clientSecret: '15be5897c8b42f268e69a63df4c57799'
+	},
+	google:{
+		clientID: '576732360882-qe3vpq5qltr1rkq9fmo6gum76j8cpdhf.apps.googleusercontent.com',
+		clientSecret: 'UXKQcjSxsaVpPuW9wywOiora'
+		
+	}
+}
